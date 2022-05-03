995e84550f3b59f3bde776c85fc7587a44f26e77
nstanford5 <67871053+nstanford5@users.noreply.github.com>
2022-05-03 15:49:11 -0600

    Update DeployerViews.js

--- workingVersions/views/DeployerViews.js
+++ workingVersions/views/DeployerViews.js
@@ -1,28 +1,27 @@
-import React from "react";
-import PlayerViews from "./PlayerViews";
+import React from 'react';
+import PlayerViews from './PlayerViews';
 
 const exports = {...PlayerViews};
 
+const sleep = (milliseconds) => new Promise(resolve => setTimeout(resolve, milliseconds));
 
 exports.Wrapper = class extends React.Component {
   render() {
-    const {whatContent} = this.props;
+    const {content} = this.props;
     return (
-      <div className="App">
-        <header className="App-header" id="root">
-          <h1>Fortune Reciever</h1>
-          {whatContent}
-        </header>
+      <div className="Deployer">
+        <h2>Deployer (Dealer)</h2>
+        {content}
       </div>
     );
   }
 }
 
 exports.SetWager = class extends React.Component {
-  render(){
+  render() {
     const {parent, defaultWager, standardUnit} = this.props;
     const wager = (this.state || {}).wager || defaultWager;
-    return (
+    return(
       <div>
         <input 
           type='number'
@@ -30,9 +29,9 @@ exports.SetWager = class extends React.Component {
           onChange={(e) => this.setState({wager: e.currentTarget.value})}
         /> {standardUnit}
         <br />
-        <button 
+        <button
           onClick={() => parent.setWager(wager)}
-        >Set wager</button>
+        >Set Wager Amount</button>
       </div>
     );
   }
@@ -41,9 +40,9 @@ exports.SetWager = class extends React.Component {
 exports.Deploy = class extends React.Component {
   render() {
     const {parent, wager, standardUnit} = this.props;
-    return(
+    return (
       <div>
-        Wager (pay to deploy): {wager} {standardUnit}
+        Wager: <strong>{wager}</strong> {standardUnit}
         <br />
         <button
           onClick={() => parent.deploy()}
@@ -55,22 +54,36 @@ exports.Deploy = class extends React.Component {
 
 exports.Deploying = class extends React.Component {
   render() {
-    return(
+    return (
       <div>Deploying... please wait</div>
     );
   }
 }
 
 exports.WaitingForAttacher = class extends React.Component {
+  async copyToClipboard(button) {
+    const {ctcInfoStr} = this.props;
+    navigator.clipboard.writeText(ctcInfoStr);
+    const origInnerHTML = button.innerHTML;
+    button.innerHTML = 'Copied!';
+    button.disabled = true;
+    await sleep(1000);
+    button.innerHTML = origInnerHTML;
+    button.disabled = false;
+  }
+
   render() {
     const {ctcInfoStr} = this.props;
     return (
       <div>
-        Waiting for the Fortune Teller
+        Waiting for a Player to sit at the table...
         <br /> Please give them this contract info:
         <pre className='ContractInfo'>
           {ctcInfoStr}
         </pre>
+        <button
+          onClick={(e) => this.copyToClipboard(e.currentTarget)}
+        >Copy to clipboard</button>
       </div>
     );
   }
